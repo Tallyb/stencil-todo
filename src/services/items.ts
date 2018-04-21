387bca17fdc72ba5c76973a1eebd06d273efacb3
Tally Barak <tally.barak@gmail.com>
2018-04-21 15:52:56 +0300

    Remove console log

--- src/services/items.ts
+++ src/services/items.ts
@@ -1,7 +1,6 @@
 export class ItemsService {
 
     get () {
-        console.log('CALLED GET')
         return Promise.resolve([
             {name: 'Milk the cow', done: true},
             {name: 'Buy milk', done: false}
